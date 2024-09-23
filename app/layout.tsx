0d08fd36315f7e96d39c14a79ceb7d84d4738274
Satwik Bhasin <satwikbhasin@gmail.com>
2024-09-23 14:39:47 -0400

    dependencies fix and analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
+import { Analytics } from "@vercel/analytics/react"
 
 import { Providers } from "./providers";
 
@@ -48,6 +49,7 @@ export default function RootLayout({
             </main>
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
