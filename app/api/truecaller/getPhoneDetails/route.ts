ace8ab1b499c95c03b471ecf18d6b5140dc44792
Satwik Bhasin <satwikbhasin@gmail.com>
2024-09-26 14:44:50 -0400

    fix

--- app/api/truecaller/getPhoneDetails/route.ts
+++ app/api/truecaller/getPhoneDetails/route.ts
@@ -5,15 +5,12 @@ import { CustomError } from "@/services/CustomError";
 
 export async function POST(request: Request) {
   try {
-    console.log("Received POST request");
-
     const formData = await request.formData();
     const secretKey = request.headers.get("Authorization");
     const region = request.headers.get("Region");
     const file = formData.get("file") as File;
 
     if (!file) {
-      console.log("No file uploaded");
       return NextResponse.json(
         { message: "No file uploaded" },
         { status: 400 }
@@ -21,7 +18,6 @@ export async function POST(request: Request) {
     }
 
     if (!secretKey) {
-      console.log("Authorization header is missing");
       return NextResponse.json(
         { message: "Authorization header is missing" },
         { status: 400 }
@@ -29,50 +25,56 @@ export async function POST(request: Request) {
     }
 
     if (!region) {
-      console.log("Region header is missing");
       return NextResponse.json(
         { message: "Region header is missing" },
         { status: 400 }
       );
     }
 
-    console.log("Processing CSV file");
     const result = await processCSV(file);
     const phones = result.phones;
     const limitExceeded = result.limitExceeded;
 
     if (phones.length === 0) {
-      console.log("No phone numbers found in the file");
       return NextResponse.json(
         { message: "No phone numbers found in the file" },
         { status: 400 }
       );
     }
 
-    console.log(`Found ${phones.length} phone numbers`);
+    const fetchPromises = phones.map((phone) =>
+      fetchPhoneDetails(phone, secretKey, region)
+    );
 
-    const start = Date.now();
-    const fetchPromises = phones.map((phone) => {
-      return fetchPhoneDetails(phone, secretKey, region)
-        .then((result) => {
-          console.log(
-            `Time taken for phone number ${phone}: ${Date.now() - start}ms`
-          );
-          return result;
-        })
-        .catch((error) => {
-          console.error(`Failed to fetch details for phone: ${phone}`, error);
-          return { error: error.message };
-        });
+    const results = await Promise.allSettled(fetchPromises);
+
+    for (const result of results) {
+      if (
+        result.status === "rejected" &&
+        result.reason instanceof CustomError &&
+        result.reason.statusCode === 403
+      ) {
+        return NextResponse.json(
+          { message: "Invalid Secret Key" },
+          { status: 403 }
+        );
+      }
+    }
+
+    const formattedResults = results.map((result) => {
+      if (result.status === "fulfilled") {
+        return result.value;
+      } else {
+        return { error: result.reason.message };
+      }
     });
 
-    const results = await Promise.all(fetchPromises);
-
-    console.log("Returning results");
-    return NextResponse.json({ results, limitExceeded }, { status: 200 });
+    return NextResponse.json(
+      { results: formattedResults, limitExceeded },
+      { status: 200 }
+    );
   } catch (error: any) {
     const statusCode = error instanceof CustomError ? error.statusCode : 500;
-    console.error(`Error: ${error.message}`);
     return NextResponse.json(
       { message: error.message },
       { status: statusCode }
